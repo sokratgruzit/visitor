21a91d5f115622e1aa078e89bcccca083a5d3686
sokratgruzit <tavadzed@gmail.com>
2025-08-13 15:37:35 +0400

    routes

--- src/components/dashboard/Account.tsx
+++ src/components/dashboard/Account.tsx
@@ -2,7 +2,7 @@ import styles from "./Account.module.css";
 
 export const Account = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Аккаунт</h1>
             <p>Настройки и управление профилем.</p>
         </div>
